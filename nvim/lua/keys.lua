7ea86ca0b711b60939be2c7e181611a91f3c32b6
Moritz <74018068+MoritzLue24@users.noreply.github.com>
2023-10-28 01:18:06 +0200

    Update keys.lua

--- nvim/lua/keys.lua
+++ nvim/lua/keys.lua
@@ -19,21 +19,3 @@ vim.keymap.set("n", "q", ":noh<CR>")
 
 -- Keep paste buffer when replacing
 vim.keymap.set("x", "<leader>p", "\"_dP")
-
--- vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeToggle<CR>')
--- vim.keymap.set('n', '<leader>f', '<cmd>Telescope find_files<CR>')
--- vim.keymap.set('n', '<leader>m', '<cmd>MarkdownPreview<CR>')
--- vim.keymap.set('n', '<leader>+', '<cmd>horizontal resize +10<CR>')
--- vim.keymap.set('n', '<leader>#', '<cmd>horizontal resize -10<CR>')
--- vim.keymap.set('n', '<leader>.', '<cmd>vertical resize +10<CR>')
--- vim.keymap.set('n', '<leader>-', '<cmd>vertical resize -10<CR>')
-
----------------
--- Debugging --
----------------
-
--- vim.keymap.set('n', '<F5>', '<cmd>DapContinue<CR>')
--- vim.keymap.set('n', '<F10>', '<cmd>DapStepOver<CR>')
--- vim.keymap.set('n', '<F11>', '<cmd>DapStepInto<CR>')
--- vim.keymap.set('n', '<F12>', '<cmd>DapStepOut<CR>')
--- vim.keymap.set('n', '<leader>b', '<cmd>DapToggleBreakpoint<CR>')
